02cbb1b1eca4f1b6424ed2376a9259649c59e7c5
mufassalredsignal <hmufassal@gmail.com>
2025-02-11 14:48:45 +0500

    update

--- lib/src/core/services/service.dart
+++ lib/src/core/services/service.dart
@@ -10,22 +10,22 @@ class MoyasarPaymentService {
   static Future<PaymentResponse> verifyPayment(String transactionId) async {
     PaymentResponse paymentResponse = PaymentResponse();
     try {
-      Uri paymentVerifyUrl = Uri.parse(
-          "${MoyasarPaymentControllers.moyasarV1ApiUrl}/$transactionId");
+      Uri paymentCaptureUrl = Uri.parse(
+          "${MoyasarPaymentControllers.moyasarV1ApiUrl}/$transactionId/capture");
 
       String authKey =
           'Basic ${base64Encode(utf8.encode('${paymentProvider.moyasarPaymentData.secretKey}:'))}';
       Map<String, String> headerData = {"authorization": authKey};
       http_client.Response response =
-          await http_client.get(paymentVerifyUrl, headers: headerData);
+          await http_client.post(paymentCaptureUrl, headers: headerData);
       final responseData = json.decode(response.body) ?? {};
 
       // printMeLog(
-      //     "Verify Response: $responseData  --> ${paymentProvider.moyasarPaymentData.secretKey}");
+      //     "Capture Response: $responseData  --> ${paymentProvider.moyasarPaymentData.secretKey}");
       paymentResponse = PaymentResponse.fromMap(responseData);
       return paymentResponse;
     } catch (e) {
-      // printMeLog("Verify Error: $e");
+      // printMeLog("Capture Error: $e");
       paymentResponse.paymentSource.message = e.toString();
       return paymentResponse;
     }
